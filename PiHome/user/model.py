c7168b3cc226fb29bbe1564e22d1b8b96deb152d
montbs <montbs@gmail.com>
2020-07-07 21:26:34 +0200

    Cambio del tamamño del card_ref

--- PiHome/user/model.py
+++ PiHome/user/model.py
@@ -27,7 +27,7 @@ class User(BaseDB):
         db.String(56),
         unique=True)
     password = db.Column(
-        db.String(255),
+        db.String(250),
         nullable=False)
     validated = db.Column(
         db.Boolean,
