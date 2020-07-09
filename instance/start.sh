c28dfd7bcec04770b9769630be36614d455983f3
montbs <montbs@gmail.com>
2020-07-09 17:20:57 +0200

    Actualización del comando start.sh

--- instance/start.sh
+++ instance/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Módulo que establece la metainformación del sistema
 
-export FLASK_APP="/home/montbs/Utils/Deb/TFG/PiHome/__init__.py"
+export FLASK_APP="/home/pi/TFG/TFG/PiHome/__init__.py"
 flask run
